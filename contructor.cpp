f512097f73bd4e650c194664b12508b17005a84d
icad08 <irsyadrm08@gmail.com>
2024-05-14 15:28:26 +0700

    add mahasiswa contructor

---
+++ contructor.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa{
+    public:
+    int nim;
+    string nama;
+
+    pubilc :
+    mahasiswa(){
+        nim = 0;
+       nama ="";
+    };
+
+    mahasiswa (int iNim){
+        nim=iNim;
+    };
+
+Mahasiswa (string inama){
+    nama=inama;
+};
+mahasiswa(int iNim, string iNama){
+    nim=iNim;
+    nama=iNama;
+};
+}
+int main(){
+
+}
