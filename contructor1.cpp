d5caa02bdf681ee6fe453dd3f40e6d7bfeb638cb
icad08 <irsyadrm08@gmail.com>
2024-05-14 15:04:56 +0700

    add member of class mhs

--- contructor1.cpp
+++ contructor1.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 class mahasiswa{
-    
-}
+    public:
+    static int nim;
+    int id;
+    string nama;
+
+    void setID();
+
+    void printAll();
+
+    mahasiswa (string pnama){
+        nama = pnama;
+        setID();
+    }
+};
 int main(){
 
 }
