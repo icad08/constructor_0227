ca222c48a0c7eeef1cf92cd2ddac2aa4dc34d1cb
icad08 <irsyadrm08@gmail.com>
2024-05-14 15:55:42 +0700

    add void angka

--- destructor.cpp
+++ destructor.cpp
@@ -12,7 +12,31 @@ class angka{
     void isiData();
 
 };
+angka::angka(int i){
+    panjang = i;
+    arr = new int [i];
+    isiData();
+angka::angka(){
+    cout << endl;
+    cetakData();
+    delete[]arr;
+    cout << "alamat array sudah dilepaskan" << endl;
+}
 
+void angka::cetakData(){
+    for(int i=1;i<=panjang;i++){
+        cout << i << "=" << arr[i] << endl;
+    }
+}
+void angka::isiData(){
+    for(int i=1;i<=panjang;i++){
+        cout <<i<<"=";
+        cin >> arr[i];
+    }
+    cout << endl;
+
+}
+}
 int main(){
-
+    
 }
